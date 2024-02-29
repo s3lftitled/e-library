5ba85e834e06721ddf88e01e998f05453b2aa764
Franksochan <jjaviendemonteverde@gmail.com>
2024-02-29 22:16:08 +0800

    Add functionality to show/hide profile section.

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,6 +1,10 @@
+import { useState, useEffect } from 'react'
 import './home.css'
 
 export const Home = () => {
+
+  const [ showProfileSection, setShowProfileSection ] = useState(false)
+
   return (
     <>
       <div className='home-container'>
@@ -8,15 +12,30 @@ export const Home = () => {
           <h1>Welcome to PanpacificU's E-Library</h1>
         </div>
         <div className='main-section'>
-          <div className='profile-section'>
-            <h2>Profile Section</h2>
-          </div>
+          { showProfileSection && 
+            <div className='profile-section'>
+              <h2>Profile Section</h2>
+              <button
+                className='hide-profile-section'
+                onClick={() => setShowProfileSection(false)}
+              >
+                x
+              </button>
+            </div>
+          }
           <div className='library-section'>
             <h2>Library Section</h2>
           </div>
+          {!showProfileSection && (
+          <button
+            className='show-profile-section'
+            onClick={() => setShowProfileSection(true)}
+          >
+            click to view profile section
+          </button>
+        )}
         </div>
       </div>
     </>
-    
   )
 }
